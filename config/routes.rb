a27310d2f69f1dec406922f2c1de804c664e8b4b
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-06-18 22:38:47 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   root "static_pages#landing_page"
-
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get "privacy_policy", to: "static_pages#privacy_policy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,10 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
+
+const webpack = require('webpack')
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+
     Popper: ['popper.js', 'default']
   }))
 module.exports = environment
